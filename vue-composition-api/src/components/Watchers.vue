326319a98564294c4fa478eb01a5d0e93fcbc477
suyogghate <suyogghate96@gmail.com>
2025-04-21 17:28:48 +0530

    watchers in vue js

---
+++ vue-composition-api/src/components/Watchers.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>{{ message }}</h1>
+    <input type="text" v-model="inputText" placeholder="Type something....." />
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+
+const message = ref('Hello Vue!!!')
+const inputText = ref('apple')
+
+// Watch for changes in inputText and update message accordingly
+watch(inputText, (newValue, oldValue) => {
+    // You can perform some logic here when inputText changes.
+    console.log(newValue);
+    console.log(oldValue);
+    message.value = `You typed: ${newValue}`
+});
+
+</script>
