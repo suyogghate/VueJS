a3e59e4c640d1f2bc4b8d78c9745d1570873674e
suyogghate <suyogghate96@gmail.com>
2025-04-21 17:29:40 +0530

    different lifecycle components explained in vue js

---
+++ vue-composition-api/src/components/LifeCycleComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <p>{{ message }}</p>
+    <button @click="updateMessage">Update Message</button>
+</template>
+
+<script setup>
+import { ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from 'vue';
+const message = ref('Hello')
+
+// LifeCycle Hooks
+onBeforeMount(() => {
+    console.log("Component is about to be mounted.");
+})
+
+onMounted(() => {
+    console.log("Component has been mounted.");
+})
+
+onBeforeUpdate(() => {
+    console.log("Component is about to be updated.");
+})
+
+onUpdated(() => {
+    console.log("Component has been updated!");
+})
+
+onBeforeUnmount(() => {
+    console.log("Component is about to unmounted.");
+})
+
+onUnmounted(() => {
+    console.log("Component has been unmounted.");
+})
+
+const updateMessage = () => {
+    message.value = 'Updated message';
+}
+
+</script>
