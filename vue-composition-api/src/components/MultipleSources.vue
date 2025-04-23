5ad238a65091e03ef4344e62d9718a9acafb06de
suyogghate <suyogghate96@gmail.com>
2025-04-23 14:56:17 +0530

    watching multiple sources using watch

---
+++ vue-composition-api/src/components/MultipleSources.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>{{ username }}</h1>
+    <h1>Count: {{ counter }}</h1>
+
+    <button @click="username = 'Suyog'">Change Name</button>
+    <button @click="counter++">Increment count</button>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+
+const username = ref('Jordan')
+const counter = ref(0)
+
+watch([username, counter], (newValue, oldValue) => {
+    console.log(newValue);
+    console.log(oldValue);
+})
+</script>
