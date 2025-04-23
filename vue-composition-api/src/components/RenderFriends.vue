d5dfe3f2bd8cabf50a525113b61da4cf448ef77a
suyogghate <suyogghate96@gmail.com>
2025-04-23 15:40:07 +0530

    toggling friends list using defineAsyncComponent

---
+++ vue-composition-api/src/components/RenderFriends.vue
@@ -0,0 +1,11 @@
+<template>
+    <h2 v-for="(friend, i) in friends" :key="i">
+        {{ friend }}
+    </h2>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const friends = ref(['Huxn', 'Jordan', 'Alex', 'Mike'])
+</script>
---
+++ vue-composition-api/src/components/ToggleFriends.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-if="showFriends">
+        <RenderFriends />
+    </div>
+    <button @click="toggleFriends">Toggle Friends List</button>
+</template>
+
+<script setup>
+import { ref, defineAsyncComponent } from 'vue';
+// import RenderFriends from './RenderFriends.vue';
+
+const RenderFriends = defineAsyncComponent(() => import('./RenderFriends.vue'))
+
+let showFriends = ref(false)
+
+const  toggleFriends = () => (showFriends.value = !showFriends.value)
+</script>
