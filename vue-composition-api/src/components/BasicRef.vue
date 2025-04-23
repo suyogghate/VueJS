3370dd1132a0e09489d09997a56bda7cfd58d1a1
suyogghate <suyogghate96@gmail.com>
2025-04-23 14:57:00 +0530

    basic ref in vue js

---
+++ vue-composition-api/src/components/BasicRef.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <input type="text" placeholder="Enter your name..." ref="myref">
+    </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const myRef = ref('')
+
+// console.log(myRef.value);
+
+onMounted(() => {
+    console.log(myRef.value);
+    myRef.value.focus();
+    myRef.value.value = 'something';
+})
+
+</script>
