4c3ec9d499e21e78098622af813e240a18bdb18c
suyogghate <suyogghate96@gmail.com>
2025-04-23 14:58:33 +0530

    binding ref and changing the contents of elements

---
+++ vue-composition-api/src/components/FunctionRef.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1 :ref="myRefFunc">This is initial value</h1>
+</template>
+
+<script setup>
+const myRefFunc = (el) => {
+    console.log(el.textContent);    // Reading text content
+    el.textContent = 'Text changed'
+    el.style.color = 'crimson'
+}
+</script>
