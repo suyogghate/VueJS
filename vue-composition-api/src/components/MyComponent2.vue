ce9de9ae1dd896c864a37276a5fc6fd16cedcc28
suyogghate <suyogghate96@gmail.com>
2025-04-23 15:00:17 +0530

    accessing the childs properties in parents using defineExpose()

---
+++ vue-composition-api/src/components/MyComponent2.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Count: {{ count }}</h1>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+// When using the <script setup>
+// - It's private by default
+// - A parent referencing a child won't be able to access anything
+
+// First we have to expose all the properties and methods
+// then we'd be able to use them in parent component.
+// - use (defineExpose) to expose our data for the parent to use.
+
+const count = ref(0)
+const increment = () => count.value++
+const decrement = () => count.value--
+
+defineExpose({ count, increment, decrement });
+</script>
---
+++ vue-composition-api/src/components/RefComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <MyComponent2 ref="myRef"/>
+</template>
+
+<script setup>
+import MyComponent2 from './MyComponent2.vue';
+import { onMounted, ref } from 'vue';
+const myRef = ref(null)
+
+onMounted(() => {
+    console.log(myRef.value);
+    
+})
+</script>
