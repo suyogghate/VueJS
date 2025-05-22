cc2ea050a47cb6bea0fe33f5dcbb6f050f8167a0
suyogghate <suyogghate96@gmail.com>
2025-05-22 16:46:15 +0530

    made a layout and inside it added welcome, dashboard and workout pages.

--- vuejs-full-course/src/App.vue
+++ vuejs-full-course/src/App.vue
@@ -1,8 +1,23 @@
 <script setup>
+import Welcome from './components/pages/Welcome.vue';
+import Layout from './components/layouts/Layout.vue';
+import Dashboard from './components/pages/Dashboard.vue';
+import Workout from './components/pages/Workout.vue';
+
+const selectedDisplay = 2;
 </script>
 
 <template>
-  
+  <div>
+    <Layout>
+        <!-- PAGE 1 -->
+        <Welcome v-if="selectedDisplay == 1"/>
+        <!-- PAGE 2 -->
+        <Dashboard v-if="selectedDisplay == 2"/>
+        <!-- PAGE 3 -->
+         <Workout v-if="selectedDisplay == 3"/>
+    </Layout>
+  </div>
 </template>
 
 <style scoped>
