f47dbdadd44e98cd2b1ed53095761a07e0d94a8e
suyogghate <suyogghate96@gmail.com>
2025-05-22 16:51:32 +0530

    created a layout of the app common for all pages.

--- vuejs-full-course/src/components/layouts/Layout.vue
+++ vuejs-full-course/src/components/layouts/Layout.vue
@@ -2,9 +2,63 @@
 </script>
 
 <template>
-  
+  <header>
+    <h1>SMOLGRAM</h1>
+  </header>
+  <main>
+    <slot/>
+  </main>
+  <footer>
+    <small>Created By</small>
+    <a href="https://www.github.com/suyogghate" target="_blank">
+        <img src="https://avatars.githubusercontent.com/u/91405767?v=4" alt="pfp"/>
+        <p>suyogghate</p>
+        <i class="fa-brands fa-github"></i>
+    </a>
+  </footer>
 </template>
 
 <style scoped>
+    header, footer, main {
+        padding: 1rem;
+        width: 100%;
+        max-width: 700px;
+        margin: 0 auto;
+    }
 
+    main {
+        flex: 1;
+    }
+
+    footer {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+        align-items: center;
+        padding: 2rem 0;
+        padding-bottom: 3rem;
+    }
+
+    footer a {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+        padding: 0.3rem;
+        padding-right: 0.5rem;
+        background: var(--background-muted);
+        border-radius: 4rem;
+        border: 1px solid transparent;
+        transition-duration: 200ms;
+        text-decoration: none;
+    }
+
+    footer a:hover {
+        border-color: var(--color-link);
+    }
+
+    footer a img{
+        max-width: 30px;
+        aspect-ratio: 1 / 1;
+        border-radius: 100%;
+    }
 </style>
